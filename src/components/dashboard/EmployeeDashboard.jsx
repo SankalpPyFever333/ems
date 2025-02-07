cd8c7ce48bb7da6c38c6f4c8f9bb83637f5d731a
sankalp <sankalppandey696@gmail.com>
2025-02-07 11:24:36 +0530

    working on employee dashboard

---
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import TaskListCards from './TaskListCards'
+
+const EmployeeDashboard = () => {
+      return (
+            <>
+
+                  <div className="mainDashboard ">
+                        <div className="p-15 flex flex-col h-screen bg-[#1C1C1C] ">
+                              <div className=' Header font-medium flex  justify-between items-end' >
+                                    <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >Sankalp ☺️</span></h1>
+                                    <button className='bg-red-600 text-white rounded-2xl p-3 ' >Log out</button>
+                              </div>
+                              
+                              <TaskListCards />
+                        </div>
+                  </div>
+
+            </>
+
+
+      )
+}
+
+export default EmployeeDashboard

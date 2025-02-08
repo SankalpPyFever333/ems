10298437cfb5ba4a097aff9566973179fe33f7fe
sankalp <sankalppandey696@gmail.com>
2025-02-08 09:32:30 +0530

    created others folder for different reusable components

---
+++ src/components/others/Headers.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Headers = () => {
+      return (
+            <div>
+                  <div className=' Header font-medium flex  justify-between items-end' >
+                        <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >Sankalp ☺️</span></h1>
+                        <button className='bg-red-600 text-white rounded-2xl p-3 ' >Log out</button>
+                  </div>
+            </div>
+      )
+}
+
+export default Headers

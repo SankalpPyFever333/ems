4c500619bfcfb34250ec286e92d869e34fc966d1
sankalp <sankalppandey696@gmail.com>
2025-02-08 09:33:51 +0530

    modified employee dashboard

--- src/components/dashboard/EmployeeDashboard.jsx
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TaskListCards from './TaskListCards'
 import TaskList from '../TaskList/TaskList'
+import Headers from '../others/Headers'
 
 const EmployeeDashboard = () => {
       return (
@@ -8,10 +9,7 @@ const EmployeeDashboard = () => {
 
                   <div className="mainDashboard ">
                         <div className="p-15 flex flex-col h-screen bg-[#1C1C1C] ">
-                              <div className=' Header font-medium flex  justify-between items-end' >
-                                    <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >Sankalp ☺️</span></h1>
-                                    <button className='bg-red-600 text-white rounded-2xl p-3 ' >Log out</button>
-                              </div>
+                              <Headers />
 
                               <TaskListCards />
                               <TaskList />

48e598c3e9c1aa54b3aca5ffc5a22c58e0bc8175
sankalp <sankalppandey696@gmail.com>
2025-02-11 03:07:20 +0530

    applied conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Login from './components/auth/Login'
 import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
 import AdminDashboard from './components/dashboard/AdminDashboard'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
+
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@example.com' && password == "adm") {
+      setUser("admin")
+      console.log(user)
+    }
+    else if (authData && authData.empData.find((e) => email == e.email && password == e.password)) {
+      setUser("employee")
+    }
+  }
+
   return (
     <>
-      <Login/>
-      {/* <EmployeeDashboard /> */}
-      {/* <AdminDashboard/> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }

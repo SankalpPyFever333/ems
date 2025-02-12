f203be7d204758a13a95c29c6b672623cd3739f5
sankalp <sankalppandey696@gmail.com>
2025-02-13 02:59:46 +0530

    🚀 Added prop for recieving the adminData

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -3,12 +3,13 @@ import CreateTaskPage from '../others/CreateTaskPage'
 import Headers from '../others/Headers'
 import AllTask from '../others/AllTask'
 
-const AdminDashboard = () => {
+const AdminDashboard = ({ userData }) => {
+      console.log(userData)
       return (
             <div className='h-screen w-full p-10 ' >
-                  <Headers />
+                  <Headers userData={userData} />
                   <CreateTaskPage />
-                  <AllTask/>
+                  <AllTask />
 
             </div>
       )

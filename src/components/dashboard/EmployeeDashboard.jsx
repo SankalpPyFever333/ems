8665b4d0a07de602660e16fc00992b3d634bb493
sankalp <sankalppandey696@gmail.com>
2025-02-13 02:58:03 +0530

    🚀 modifies Em dashboard

--- src/components/dashboard/EmployeeDashboard.jsx
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -3,15 +3,16 @@ import TaskListCards from './TaskListCards'
 import TaskList from '../TaskList/TaskList'
 import Headers from '../others/Headers'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({userData}) => {
+      
       return (
             <>
 
                   <div className="mainDashboard ">
                         <div className="p-15 flex flex-col h-screen bg-[#1C1C1C] ">
-                              <Headers />
-                              <TaskListCards />
-                              <TaskList />
+                              <Headers userData = {userData}/>
+                              <TaskListCards userData={userData} />
+                              <TaskList userData={userData} />
                         </div>
                   </div>
 

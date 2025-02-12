2b325c2e459f6b6acb4b08cbd7264f6d09d43dd8
sankalp <sankalppandey696@gmail.com>
2025-02-13 03:00:55 +0530

    🚀 showing name of employe or admin who is currently logged in

--- src/components/others/Headers.jsx
+++ src/components/others/Headers.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Headers = () => {
+const Headers = ({ userData }) => {
+      console.log(userData.firstName)
       return (
             <div>
                   <div className=' Header font-medium flex  justify-between items-end' >
-                        <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >Sankalp ☺️</span></h1>
+                        <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >{userData.firstName} ☺️</span></h1>
                         <button className='bg-red-600 text-white rounded-2xl p-3 ' >Log out</button>
                   </div>
             </div>

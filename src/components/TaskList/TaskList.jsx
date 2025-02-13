401cfcff9ad10eb2a392e75b3a64d7f60ccde276
sankalp <sankalppandey696@gmail.com>
2025-02-14 03:45:34 +0530

    🚀 conditionally rendering all the components🔥

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,64 +1,30 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
+import NewTask from './NewTask'
 
-const TaskList = () => {
+const TaskList = ({ userData }) => {
+      console.log(userData)
       return (
             <div id='taskList' className='h-[55%] overflow-x-auto w-full  mt-10 flex flex-nowrap p-5 items-center gap-5 justify-start ' >
-                  <div className='w-[300px] flex-shrink-0 h-full bg-emerald-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-red-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-blue-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-red-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-green-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-yellow-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
-                  <div className='w-[300px] flex-shrink-0 h-full bg-red-400 rounded-xl p-5 ' >
-                        <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
-                        </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
-                  </div>
+                  {
+                        userData.tasks.map((elem) => {
+                              if (elem.active) {
+                                    return <AcceptTask />
+                              }
+                              if (elem.new_task) {
+                                    return <NewTask />
+                              }
+                              if (elem.completed) {
+                                    return <CompleteTask />
+                              }
+                              if (elem.failed) {
+                                    return <FailedTask />
+                              }
+                        })
+                  }
+
             </div>
       )
 }

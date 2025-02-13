9792e334cea8986bf7b53700d952adc023577d7f
sankalp <sankalppandey696@gmail.com>
2025-02-14 03:44:36 +0530

    🚀 user can mark a task as completed

---
+++ src/components/TaskList/CompleteTask.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const CompleteTask = () => {
+      return (
+            <div>
+                  <div className='w-[300px] flex-shrink-0 h-full bg-blue-400 rounded-xl p-5 ' >
+                        <div className='flex items-center justify-between ' >
+                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
+                              <h4 className='text-sm' >20 feb 2024</h4>
+                        </div>
+                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
+                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
+                        <div className='mt-5 ' >
+                              <button className='bg-pink-500 px-2 py-2 mt-5 rounded-2xl '  >Complete</button>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default CompleteTask

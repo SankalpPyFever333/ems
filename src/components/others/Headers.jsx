644c2507796931f8b1ed9cd93b7bd0fa23e5d282
sankalp <sankalppandey696@gmail.com>
2025-02-16 00:28:07 +0530

    🔥 added optional chaining and nullish coalescing

--- src/components/others/Headers.jsx
+++ src/components/others/Headers.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const Headers = ({ userData }) => {
-      // console.log(userData.firstName)
-      // console.log(userData)
+const Headers = () => {
       const name = JSON.parse(localStorage.getItem('loggedUserData'));
-      console.log(name.adminData)
+      const firstName = name?.adminData?.firstName ?? name?.emp?.firstName ?? "User"
       const handleLogOut = () => {
             localStorage.removeItem("loggedUser");
             localStorage.removeItem("loggedUserData");
@@ -12,7 +10,7 @@ const Headers = ({ userData }) => {
       return (
             <div>
                   <div className=' Header font-medium flex  justify-between items-end' >
-                        <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >{userData.firstName} ☺️</span></h1>
+                        <h1 className='text-xl' >Hello , <br /> <span className='text-3xl font-semibold ' >{firstName} ☺️</span></h1>
                         <button onClick={handleLogOut} className='bg-red-600 text-white rounded-2xl p-3 ' >Log out</button>
                   </div>
             </div>

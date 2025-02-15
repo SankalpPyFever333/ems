b405873378e7183a6edd2c0041907579b6c7d0fb
sankalp <sankalppandey696@gmail.com>
2025-02-16 01:05:24 +0530

    added data props in failed and newtask.jsx

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const FailedTask = () => {
+const FailedTask = ({data}) => {
       return (
             <div>
                   <div className='w-[300px] flex-shrink-0 h-full bg-blue-400 rounded-xl p-5 ' >
                         <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
+                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >{data.category}</h3>
+                              <h4 className='text-sm' >{data.date}</h4>
                         </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
+                        <h2 className='mt-5 text-2xl font-semibold' >{data.task_title}</h2>
+                        <p className='text-sm mt-2 ' >{data.description}</p>
                         <div className='mt-5 ' >
                               <button className='bg-red-500 px-2 py-2 mt-5 rounded-2xl '  >Failed Task</button>
                         </div>
--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const NewTask = () => {
+const NewTask = ({ data }) => {
       return (
             <div>
                   <div className='w-[300px] flex-shrink-0 h-full bg-yellow-400 rounded-xl p-5 ' >
                         <div className='flex items-center justify-between ' >
-                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-3 py-1 ' >High</h3>
-                              <h4 className='text-sm' >20 feb 2024</h4>
+                              <h3 className='text-sm  bg-red-600 text-white rounded-xl font-semibold px-1 py-1 ' >{data.category}</h3>
+                              <h4 className='text-sm' >{data.date}</h4>
                         </div>
-                        <h2 className='mt-5 text-2xl font-semibold' >Practice DSA Striver's</h2>
-                        <p className='text-sm mt-2 ' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis quo nobis?</p>
+                        <h2 className='mt-2 text-2xl font-semibold' >{data.task_title}</h2>
+                        <p className='text-sm mt-2 ' >{data.description}</p>
                         <div className='mt-5 ' >
                               <button className='bg-emerald-500 px-2 py-2 mt-5 rounded-2xl '  >Accept Task</button>
                         </div>
